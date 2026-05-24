72273a5b1dd1725e669e186b357cd65fd513d648
Vanshika Vanya <btech10314.24@bitmesra.ac.in>
2026-05-24 22:49:14 +0530

    solution to 1857 A

---
+++ p1857A.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        int odd_sum=0,even_sum=0;
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+            if(a[i]%2==0){
+                even_sum+=a[i];
+            }
+            else{
+                odd_sum+=a[i];
+            }
+        }
+        if(odd_sum%2==0 && even_sum%2==0){
+            cout << "YES\n";
+        }
+        else{
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}
