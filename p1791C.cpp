883462d8b936274bc84547f4040a752335c7079a
Vanshika Vanya <btech10314.24@bitmesra.ac.in>
2026-05-26 22:47:06 +0530

    solution to 1791 C

---
+++ p1791C.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        int i = 0, j = n - 1;
+        while (i < j) {
+            if(s[i]!=s[j]){
+                n-=2;
+                i++;
+                j--;
+            }
+            else break;
+        }
+        cout << n << "\n";
+    }
+    return 0;
+}
