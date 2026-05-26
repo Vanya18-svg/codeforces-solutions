7478b391845b1c17ee163f602aef6cefb44c27cb
Vanshika Vanya <btech10314.24@bitmesra.ac.in>
2026-05-26 22:40:31 +0530

    solution to 1761 A

---
+++ p1761A.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--){
+        int n,a,b;
+        cin >> n >> a >> b;
+        if(n==a && n==b){
+            cout<<"Yes"<<endl;
+        } else if((n-(a+b))>=2){
+            cout<<"Yes"<<endl;
+        } else {
+            cout<<"No"<<endl;
+        }
+    }
+    return 0;
+}
