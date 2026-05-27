6b2342c1ce189199a68db2e36665fb15d1e16452
Vanshika Vanya <btech10314.24@bitmesra.ac.in>
2026-05-27 22:25:34 +0530

    solution to 1869 A

---
+++ p1869A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t; 
+	while (t--)
+	{
+		long long n;
+		cin >> n; 
+		long long a[n];
+		for (int i = 0; i < n; i++) 
+			cin >> a[i];
+		if (n % 2 == 0) 
+		{
+			cout << 2 << endl; // Output the number of operations
+			cout << 1 << " " << n << endl; // Perform operation on the entire array
+			cout << 1 << " " << n << endl; // Repeat the operation
+		}
+		else 
+		{
+			cout << 4 << endl; // Output the number of operations
+			cout << 1 << " " << n - 1 << endl; // Perform operation on the first n-1 elements
+			cout << 1 << " " << n - 1 << endl; // Repeat the operation
+			cout << n - 1 << " " << n << endl; // Perform operation on the last two elements
+			cout << n - 1 << " " << n << endl; // Repeat the operation
+		}
+	}
+	return 0;
+}
+
