c9d7ca3d029e366079448a525661edbf090acbde
Vanshika Vanya <btech10314.24@bitmesra.ac.in>
2026-06-01 22:43:42 +0530

    solution to 1537 B

---
+++ p1537B.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T;
+    cin >> T;
+    while (T--) {
+        long long n,m,i,j;
+        cin >> n >> m >> i >> j;
+        cout << 1 << " " << 1 << " " << n << " " << m << endl;
+    }
+    return 0;
+}
